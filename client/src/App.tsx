73a7a0a803503fab0ddcffb57d8d01c535bbb8fa
Akshat Dubey <akshatdubey0808@gmail.com>
2025-02-04 12:33:49 +0530

    scaffolded vite react app with tailwind v4 and custom font

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center font-primary font-bold text-5xl tracking-tight justify-center font-lex">
+      Hello
+    </div>
+  );
+}
+
+export default App;
